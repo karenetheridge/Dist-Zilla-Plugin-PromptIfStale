c2738598f9f51ac3404c5931d7426143580b9b32
Karen Etheridge <ether@cpan.org>
2013-08-11 20:46:09 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.017, github profile.

---
+++ lib/Dist/Zilla/Plugin/PromptIfStale.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+package Dist::Zilla::Plugin::PromptIfStale;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::PromptIfStale;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-PromptIfStale>
+(or L<bug-Dist-Zilla-Plugin-PromptIfStale@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-PromptIfStale@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use Test::Warnings;
+use Dist::Zilla::Plugin::PromptIfStale;
+
+fail('this test is TODO!');
+
+done_testing;
