9d80164147610c249eb5733cbd219620a0a9ddbd
Karen Etheridge <ether@cpan.org>
2014-01-24 18:13:13 -0800

    add vim modeline for aiding contributors

--- lib/Dist/Zilla/Plugin/PromptIfStale.pm
+++ lib/Dist/Zilla/Plugin/PromptIfStale.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::PromptIfStale;
 # ABSTRACT: Check at build/release time if modules are out of date
+# vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
 with 'Dist::Zilla::Role::BeforeBuild',
