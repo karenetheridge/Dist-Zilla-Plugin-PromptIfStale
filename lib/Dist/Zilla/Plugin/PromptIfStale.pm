635a9d920fd8ef017ab1f715165fe89fc4513e3c
Karen Etheridge <ether@cpan.org>
2014-07-05 10:12:57 -0700

    add keywords to metadata

--- lib/Dist/Zilla/Plugin/PromptIfStale.pm
+++ lib/Dist/Zilla/Plugin/PromptIfStale.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::PromptIfStale;
 # ABSTRACT: Check at build/release time if modules are out of date
+# KEYWORDS: prerequisites upstream dependencies modules metadata update stale
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
