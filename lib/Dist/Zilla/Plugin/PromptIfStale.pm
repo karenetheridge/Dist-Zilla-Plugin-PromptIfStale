936ca7735870b5e50b16df168c8442c7c8f6bbeb
Karen Etheridge <ether@cpan.org>
2023-04-30 11:50:52 +0200

    DynaLoader is no longer indexed
    It was previously indexed to N/NW/NWCLARK/perl-5.8.6.tar.gz, because it has been dynamically generated since then.

--- lib/Dist/Zilla/Plugin/PromptIfStale.pm
+++ lib/Dist/Zilla/Plugin/PromptIfStale.pm
@@ -192,7 +192,7 @@ sub stale_modules
         next if $already_checked{$module};
 
         # these core modules should be indexed, but aren't
-        if (any { $module eq $_ } qw(Config DB Errno Pod::Functions))
+        if (any { $module eq $_ } qw(Config DB DynaLoader Errno Pod::Functions))
         {
             $self->log_debug([ 'skipping core module: %s', $module ]);
             $already_checked{$module}++;
