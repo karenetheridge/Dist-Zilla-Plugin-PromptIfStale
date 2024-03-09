7999bd71c2f4e78d3df050e94c513159b31463f7
Karen Etheridge <ether@cpan.org>
2024-03-09 14:51:50 -0800

    use signatures and postfix deref, bumping minimum perl to 5.020

--- lib/Dist/Zilla/Plugin/EnsureNotStale.pm
+++ lib/Dist/Zilla/Plugin/EnsureNotStale.pm
@@ -8,6 +8,14 @@ our $VERSION = '0.059';
 
 use Moose;
 extends 'Dist::Zilla::Plugin::PromptIfStale';
+
+use strictures 2;
+use stable 0.031 'postderef';
+use experimental 'signatures';
+use if "$]" >= 5.022, experimental => 're_strict';
+no if "$]" >= 5.031009, feature => 'indirect';
+no if "$]" >= 5.033001, feature => 'multidimensional';
+no if "$]" >= 5.033006, feature => 'bareword_filehandles';
 use namespace::autoclean;
 
 has '+fatal' => (
